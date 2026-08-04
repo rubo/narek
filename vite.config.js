96583ba3c438a1b483da4d474d8a78b10b1bbcaa
Ruben Buniatyan <337518+rubo@users.noreply.github.com>
2026-08-04 15:49:05 +0200

    chore: update packages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import babel from '@rolldown/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import books from './scripts/vite-plugin-books';
+import books from './scripts/vite-plugin-books.js';
 
 // https://vite.dev/config/
 export default defineConfig({
